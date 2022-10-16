96da8f51d3bd5eee38b71859f04a6bdc56efd599
Robinson_Xavier <robinson.webzen@gmail.com>
2022-10-16 16:51:09 -0300

    added POST signup function

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt';
 import {signupSchema, singinSchema} from '../schemas/authSchema.js';
 
 async function signupAccount (req, res) {
-    const {username, email, password} = req.body;
+    const {username, email, password, confirmPassword} = req.body;
 
     const validation = signupSchema.validate(req.body, {abortEarly: false});
 
@@ -12,9 +12,24 @@ async function signupAccount (req, res) {
         return res.status(422).send(errors);
     }
 
+    if(confirmPassword !== password) {
+        return res.status(422).send('Password and Confirm Password must be the same');
+    }
+
     try {
+
+        const findEmail = await connection.query(`SELECT * FROM users WHERE email = $1`, [email]);
         
+        if (findEmail.rows[0]) {
+            return res.sendStatus(409);
+        }
+
+        const hashPassword = bcrypt.hashSync(password, 10);
+
+        await connection.query(`INSERT INTO users (username, email, password) VALUES ($1, $2, $3)`, [username, email, hashPassword]);
         
+        res.sendStatus(201);
+
     } catch (error) {
         console.log(error)
         return res.sendStatus(500)
--- src/schemas/authSchema.js
+++ src/schemas/authSchema.js
@@ -3,7 +3,8 @@ import joi from 'joi';
 const signupSchema = joi.object({
     username: joi.string().required().min(3).max(25),
     email: joi.string().email({minDomainSegments: 2, tlds: { allow: ['com', 'net']}}).required(),
-    password: joi.string().min(8).required()
+    password: joi.string().min(8).required(),
+    confirmPassword: joi.string().min(8).required()
 });
 
 const singinSchema = joi.object({
