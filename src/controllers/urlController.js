5f7777883877b106e81e76b31036be509b0a6298
Robinson_Xavier <robinson.webzen@gmail.com>
2022-10-16 23:28:43 -0300

    added accessUrl function

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -27,8 +27,8 @@ async function postUrl (req, res) {
             return res.sendStatus(401);
         }
         
-        await connection.query(`INSERT INTO shorteneds ("userId", url, "shortUrl") VALUES ($1, $2, $3);`, 
-        [session.rows[0].userId, url, shortened]);
+        await connection.query(`INSERT INTO shorteneds ("userId", url, "shortUrl", accessed) VALUES ($1, $2, $3, $4);`, 
+        [session.rows[0].userId, url, shortened, 0]);
 
         res.status(201).send({
             shortUrl: shortened
@@ -64,5 +64,28 @@ async function getUrl (req, res) {
     }
 };
 
+async function acessUrl(req, res) {
+    const {shortUrl} = req.params;
 
-export {postUrl , getUrl};
+    try {
+        const searchUrl = connection.query(`SELECT * FROM shorteneds where "shortUrl" = $1;`, [shortUrl]);
+
+        if(!searchUrl.rows[0]) {
+            return res.sendStatus(404);
+        }
+
+        const addOne = Number(searchUrl.rows[0].accessed) + 1;
+
+        await connection.query(`UPDATE shorteneds SET accessed = $1 WHERE id = $2;`, [addOne, searchUrl.rows[0].id]);
+
+        res.status(200);
+        res.redirect(searchUrl.rows[0].url);
+        
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(500)
+    }
+};
+
+
+export {postUrl , getUrl, acessUrl};
--- src/routers/urlRouter.js
+++ src/routers/urlRouter.js
@@ -1,9 +1,14 @@
 import express from 'express';
 
-import { postUrl } from '../controllers/urlController.js';
+import { postUrl, getUrl, acessUrl } from '../controllers/urlController.js';
 
 const urlRouter = express.Router();
 
 urlRouter.post('/urls/shorten', postUrl);
+
+urlRouter.post('/urls/:id', getUrl);
+
+urlRouter.post('/urls/open/:shortUrl', acessUrl);
+
 
 export default urlRouter;
